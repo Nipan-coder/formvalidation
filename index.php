b49ca32bb9c0e4713ca11b0ab7f957ea5a1e716f
Nipan-coder <67820736+Nipan-coder@users.noreply.github.com>
2021-04-02 23:07:09 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <title>Form</title>
+</head>
+<body>
+
+
+  <section class="my-5">
+    <h2 class="text-center">Contact Us</h2>
+    <div class="container py-5">
+      <div class="row">
+          <div class="col-md-10 mx-auto">
+              <form>
+                  <div class="form-group row">
+                      <div class="col-sm-6">
+                          <label >Name</label>
+                          <input type="text" class="form-control"  placeholder="Enter name">
+                      </div>
+                      <div class="col-sm-6">
+                          <label >Email Address</label>
+                          <input type="email" class="form-control"  placeholder="Enter Email address">
+                      </div>
+                  </div>
+                  <div class="form-group row">
+                      <div class="col-sm-6">
+                          <label >Phone Number</label>
+                          <input type="mobile" class="form-control"  placeholder="Enter Phone Number">
+                      </div>
+                      <div class="col-sm-6">
+                          <label >Birthday</label>
+                          <input type="date" data-date-format="dd/mm/yy" class="form-control"  placeholder="Enter Birthday">
+                      </div>
+                  </div>
+                  <div class="form-group row">
+                   
+                    <div class="group col">
+                      <p>Gender</p>
+                      <div class="form-check">
+                        <input class="form-check-input" type="radio" name="exampleRadios"  value="option1" checked>
+                        <label class="form-check-label" >
+                          Male
+                        </label>
+                      </div>
+                      <div class="form-check">
+                        <input class="form-check-input" type="radio" name="exampleRadios"  value="option2" checked>
+                        <label class="form-check-label" >
+                          Female
+                        </label>
+                      </div>
+
+                    </div>
+                      
+
+                      <div class="col-sm-6 ">
+                        <label >Questions</label>
+                        <textarea name="comments" class="form-control" placeholder="Enter Questions"></textarea>
+                    </div>
+                    
+                </div>
+                
+                     
+                      
+                 <center><button type="button" class="btn btn-primary ">Submit</button></center> 
+              </form>
+              
+          </div>
+      </div>
+  </div>
+  <br> <br>
+  <footer>
+    <p class="p-lg-0 bg-light text-primary text-center"> Copyrights @2021 By Nipan Pradhan.</p>
+    </footer>
+    </section>
+    
+    
+
+
+
+
+
+
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
